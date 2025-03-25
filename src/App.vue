f6c7b846a6bc0c6691ff7f0ac81086821bc0504a
Lisa Taylor <ltaylor.lisa@gmail.com>
2025-03-25 07:10:35 -0700

    add flag assets and styling

--- src/App.vue
+++ src/App.vue
@@ -8,18 +8,29 @@
     .header-details
       .flag-pole
       .flags-wrapper
-        .adventure-flag
-          img(src="@/assets/flags/rat-large.svg" alt="Rat Dungeon Flag")
+        .adventure-flag.rat
+          img(v-if="dateAfter('apr')" src="@/assets/flags/rat-large.svg" alt="Rat Dungeon Flag")
+          img(v-else src="@/assets/flags/rolled-flag-rat.svg" alt="Rat Dungeon Flag")
         .adventure-flag
           .rolled-up-flag.snail
+            img(v-if="dateAfter('may')" src="@/assets/flags/snail-flag-large.svg" alt="Snail Dungeon Flag")
+            img(v-else src="@/assets/flags/rolled-flag-snail.svg" alt="Future Dungeon Flag")
         .adventure-flag
-          .rolled-up-flag.snail
+          .rolled-up-flag.ladder
+            img(v-if="dateAfter('jun')" src="@/assets/flags/ladder-flag-large.svg" alt="Ladder Flag")
+            img(v-else src="@/assets/flags/rolled-flag-ladder.svg" alt="Future Dungeon Flag")
         .adventure-flag
-          .rolled-up-flag.fire
+          .rolled-up-flag.feast
+            img(v-if="dateAfter('jul')" src="@/assets/flags/feast-flag-large.svg")
+            img(v-else src="@/assets/flags/rolled-flag-feast.svg" alt="Future Dungeon Flag")
         .adventure-flag
           .rolled-up-flag.benny
+            img(v-if="dateAfter('aug')" src="@/assets/flags/benny-flag-large.svg")
+            img(v-else src="@/assets/flags/rolled-flag-benny.svg" alt="Future Dungeon Flag")
         .adventure-flag
-          .rolled-up-flag.deepfake
+          .rolled-up-flag.final
+            img(v-if="dateAfter('sep')" src="@/assets/flags/final-flag-large.svg")
+            img(v-else src="@/assets/flags/rolled-flag-final.svg" alt="Future Dungeon Flag")
       .nav-wrapper
         button.nav-button
           RouterLink(to="/") guide
@@ -58,12 +69,32 @@ export default defineComponent({
   setup() {
     const appStore = useAppStore()
 
+    const dateAfter = (month: string) => {
+      const monthMap = {
+        jan: 0,
+        feb: 1,
+        mar: 2,
+        apr: 3,
+        may: 4,
+        jun: 5,
+        jul: 6,
+        aug: 7,
+        sep: 8,
+        oct: 9,
+        nov: 10,
+        dec: 11,
+      }
+      const currentMonth = new Date().getMonth()
+      return monthMap[month] <= currentMonth
+    }
+
     onMounted(() => {
       appStore.fetchChallengeData()
     })
 
     return {
       appStore,
+      dateAfter,
     }
   },
 })
@@ -111,32 +142,35 @@ export default defineComponent({
         }
         .rolled-up-flag {
           width: 100px;
-          height: 20px;
-          border-top-left-radius: 8px;
-          border-bottom-left-radius: 8px;
-          border-bottom-right-radius: 8px;
-          box-shadow: 4px 0px 0px 0px #676767;
         }
-        .snail {
-          background-color: #3df0fa;
-          border: 2px solid #17c2ce;
-        }
-        .fire {
-          background-color: #17c2ce;
-          border: 2px solid #3df0fa;
-        }
-        .feast {
-          background-color: #d4bcfe;
-          border: 2px solid #a47cdb;
-        }
-        .benny {
-          background-color: #a47cdb;
-          border: 2px solid #d4bcfe;
-        }
-        .deepfake {
-          background-color: #fc795c;
-          border: 2px solid #fec5b2;
-        }
+        // .rolled-up-flag-css {
+        //   width: 100px;
+        //   height: 20px;
+        //   border-top-left-radius: 8px;
+        //   border-bottom-left-radius: 8px;
+        //   border-bottom-right-radius: 8px;
+        //   box-shadow: 4px 0px 0px 0px #676767;
+        // }
+        // .snail {
+        //   background-color: #3df0fa;
+        //   border: 2px solid #17c2ce;
+        // }
+        // .ladder {
+        //   background-color: #17c2ce;
+        //   border: 2px solid #3df0fa;
+        // }
+        // .feast {
+        //   background-color: #d4bcfe;
+        //   border: 2px solid #a47cdb;
+        // }
+        // .benny {
+        //   background-color: #a47cdb;
+        //   border: 2px solid #d4bcfe;
+        // }
+        // .final {
+        //   background-color: #fc795c;
+        //   border: 2px solid #fec5b2;
+        // }
       }
     }
     .nav-wrapper {
